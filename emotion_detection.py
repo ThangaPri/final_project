a46b1273632988b34b970bf5956b65fa9b56b9ef
ThangaPri <priya.thangam2023@gmail.com>
2024-07-26 02:38:07 -0400

    Modified emotion detection file with formatted output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,6 @@
 """Module import the request library."""
 # Import the requests library to handle HTTP requests
-import requests
+import requests,json
 
 ' ' 'Demonstrates the extracting the finer emotions like joy' ' ' 
 # Define a function named emotion_detector that takes a string input (text_to_analyse)
@@ -17,6 +17,30 @@ def emotion_detector(text_to_analyze):
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
 
-    # Return the response text from the API
-    return response.text  
+    #format the response
+    
+    formatted_response=json.loads(response.text)
+
+    #Extract required set if emotionns
+    anger   =   formatted_response['emotionPredictions'][0]['emotion']['anger']
+
+    disgust =   formatted_response['emotionPredictions'][0]['emotion']['disgust']
+
+    fear    =   formatted_response['emotionPredictions'][0]['emotion']['fear']
+
+    joy     =   formatted_response['emotionPredictions'][0]['emotion']['joy']
+
+    sadness =   formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    
+
+    #logic to find dominant emotion which has highest score
+    emotions    =   {'anger': anger, 'disgust': disgust,'fear': fear,'joy': joy,'sadness': sadness}
+
+    dominant_emotion    =   max(emotions, key= lambda x: emotions[x])
+    
+    #return the result as outout
+    return {'anger': anger, 'disgust': disgust,'fear': fear,'joy': joy,'sadness': sadness,'dominant_emotion':dominant_emotion}
+
+    
+
 
